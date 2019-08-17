8694dfdfc2d7a62dbcc014af933283e19954e8af
Ernesto Copello <ernesto.copello@fractalindustries.com>
2019-08-17 12:03:55 -0300

    Replaced equal match by matching test macros

--- test/hacker_stories_api_SUITE.erl
+++ test/hacker_stories_api_SUITE.erl
@@ -3,23 +3,26 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-% test known story
+% test fixed known story
 check_known_story_test() -> 
-    {ok, #{<<"by">> := <<"pg">>,
-	   <<"descendants">> := 15,
-	   <<"id">> := 1,
-	   <<"type">> := <<"story">>,
-	   <<"kids">> := [15,234509,487171,454426,454424,454410,82729],
-	   <<"score">> := 57,<<"time">> := 1160418111,
-	   <<"title">> := <<"Y Combinator">>,
-	   <<"url">> := <<"http://ycombinator.com">>}} = hacker_stories_api:get_story(1).
+    ?assertMatch(
+       {ok, #{<<"by">> := <<"pg">>,
+	      <<"descendants">> := 15,
+	      <<"id">> := 1,
+	      <<"type">> := <<"story">>,
+	      <<"kids">> := [15,234509,487171,454426,454424,454410,82729],
+	      <<"score">> := 57,<<"time">> := 1160418111,
+	      <<"title">> := <<"Y Combinator">>,
+	      <<"url">> := <<"http://ycombinator.com">>}},
+       hacker_stories_api:get_story(1)).
 
 %% test hacker news service timeout
 check_timeout_error_test() ->
     meck:new(httpc, [non_strict]),
     meck:expect(httpc, request, fun(_,_,_,_) -> {error, timeout} end),
-    error = hacker_stories_api:get_story(1),
-    meck:unload(httpc).
+    Result = hacker_stories_api:get_story(1),
+    meck:unload(httpc),
+    ?assertMatch(error, Result).
 
 %% test wrong JSON format
 check_json_format_error_test() ->
@@ -37,8 +40,10 @@ check_json_format_error_test() ->
 					       "max-age=31556926; includeSubDomains; preload"}],
 					     "{\"by\":\"pg\""}}
 				end),
-    error = hacker_stories_api:get_story(1),
-    meck:unload(httpc).
+    Result = hacker_stories_api:get_story(1),
+    meck:unload(httpc),
+    ?assertMatch(error, Result).
+
     
     
 
