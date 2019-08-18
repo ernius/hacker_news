46c79cc74a0f125c2237e2e4a11816db4a9705eb
Ernesto Copello <ernesto.copello@fractalindustries.com>
2019-08-18 12:46:59 -0300

    Return all stories if no page query parameter is found

--- src/handlers/hacker_top_stories_handler.erl
+++ src/handlers/hacker_top_stories_handler.erl
@@ -8,16 +8,22 @@ init(Req=#{method := <<"GET">>}, Opts) ->
     try
 	lager:info("Top Stories handler"),
 	QsVals = cowboy_req:parse_qs(Req),
-	{_, Page} = lists:keyfind(<<"page">>, 1, QsVals),
-	lager:info("Page requested: ~p",[Page]),
-	{ok, PagesNumber, Stories} = hacker_stories_fetch_service:get_paginated_stories(list_to_integer(binary_to_list(Page))),
-	{ok, 
-	 hacker_stories_web:reply_ok(Req, 
-				     #{ <<"page">> => Page, 
-					<<"total_pages">> => PagesNumber, 
-					<<"page_size">> => ?PAGINATION_PAGE_SIZE, 
-					<<"stories">> => Stories }), 
-	 Opts}
+	case lists:keyfind(<<"page">>, 1, QsVals) of
+	    {_, Page} ->	
+		lager:info("Page requested: ~p",[Page]),
+		{ok, PagesNumber, Stories} = hacker_stories_fetch_service:get_paginated_stories(list_to_integer(binary_to_list(Page))),
+		{ok, 
+		 hacker_stories_web:reply_ok(Req, 
+					     #{ <<"page">> => Page, 
+						<<"total_pages">> => PagesNumber, 
+						<<"page_size">> => ?PAGINATION_PAGE_SIZE, 
+						<<"stories">> => Stories }), 
+		 Opts};
+	    false ->
+		lager:info("Requested all top stories"),
+		{ok, Stories} = hacker_stories_fetch_service:get_stories(),
+		{ok, hacker_stories_web:reply_ok(Req, Stories), Opts}
+	end
     catch
 	error:badarg ->
 	    lager:error("Bad query parameter"),
